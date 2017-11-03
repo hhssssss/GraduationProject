db60b6234ce43b08cfb5a276f086f43a1a46ada6
Unknown <hhssssssss@163.com>
2017-11-03 14:40:02 +0800

    任务书文档上传

--- vue/src/components/el_header.vue
+++ vue/src/components/el_header.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="javascript" v-if="loginFlag">您还未登录!</a>
-            <a class="navbar-brand" href="javascript" v-else="">欢迎您！&nbsp&nbsp{{userId}}&nbsp&nbsp</a>
-          </div>
-          <button type="button" class="btn btn-primary navbar-btn" @click="loginShow" v-if="loginFlag">登录</button>
-          <button type="button" class="btn btn-primary navbar-btn" @click="loginOut" v-else="">注销</button>
+      <div class="container">
+        <img src="../assets/logo.png" alt="" class="logo">
+        <div class="control">
+          <a  href="javascript" v-if="loginFlag">您还未登录!</a>
+          <a  href="javascript" v-else="">欢迎您！&nbsp&nbsp{{userId}}&nbsp&nbsp</a>
+
+          <div  class='button' @click="loginShow" v-if="loginFlag">登录</div>
+          <div  class='button' @click="loginOut" v-else="">注销</div>
         </div>
-      </nav>
+      </div>
     </div>
 </template>
 
@@ -43,4 +43,36 @@
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .container{
+    width: 100%;
+    height: 140px;
+    padding: 0 10%;
+    display: flex;
+    align-items: center;
+  }
+  .logo{
+    height: 50px;
+    width: auto;
+  }
+  .control{
+    flex-grow: 1;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  .button{
+    height: 40px;
+    width: 120px;
+    background-color: #08aba6;
+    color: white;
+    text-align: center;
+    line-height: 40px;
+    transition: all 0.3s ease-in-out;
+  }
+  .button:hover{
+    background-color: #fff;
+    color: #08aba6;
+  }
+
 </style>
